82258a7f21fbc4b2e43f506ba678bd7c65a2c3dd
philippjohannsen <philipp-vincent.johannsen@rsyd.dk>
2024-05-12 12:36:51 +0200

    created notebook

--- src/data/drive.py
+++ src/data/drive.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pickle
 import json
 from datetime import datetime, timedelta
-from config import DATA_DIR, RAW_DIR
+from .config import DATA_DIR, RAW_DIR
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
